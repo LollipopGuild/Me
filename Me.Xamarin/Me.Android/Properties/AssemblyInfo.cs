35e89de58fddd4ec9e00fad8dc9f3d5719b2a7ac
alcheByte <byron@alchebyte.com>
2019-10-29 16:57:13 -0500

    design: Android tweaks

--- Me.Xamarin/Me.Android/Properties/AssemblyInfo.cs
+++ Me.Xamarin/Me.Android/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Me.Xamarin.Android")]
+[assembly: AssemblyTitle("Me.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Me.Xamarin.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Me.Android")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
