7096cd56bad319c4ba08da270eea5b49c2b95331
alcheByte <byron@alchebyte.com>
2019-10-29 17:10:39 -0500

    design: Android tweaks

--- Me.Xamarin/Me.Android/MainActivity.cs
+++ Me.Xamarin/Me.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Me.Android
 {
-    [Activity(Label = "Me.Forms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Me?", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
