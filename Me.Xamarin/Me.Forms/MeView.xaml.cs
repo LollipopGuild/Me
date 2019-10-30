05fffc7377b2ea549de05dc069b6f6fdf4bbef20
alcheByte <byron@alchebyte.com>
2019-10-30 09:57:25 -0500

    design: make MeView reactive

--- Me.Xamarin/Me.Forms/MeView.xaml.cs
+++ Me.Xamarin/Me.Forms/MeView.xaml.cs
@@ -6,12 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using ReactiveUI.XamForms;
+
 namespace Me.Forms
 {
+    public class MeView_Reactive : ReactiveContentPage<MeViewModel> { }
+
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MeView : ContentPage
+    public partial class MeView : MeView_Reactive
     {
         public MeView()
         {
---
+++ Me/MeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Me
+{
+    public class MeViewModel : ReactiveObject
+    {
+        [Reactive]
+        public Byte[] Picture { get; set; }
+
+        [Reactive]
+        public string[] GivenNames { get; set; }
+
+        [Reactive]
+        public string FamilyName { get; set; }
+    }
+}
