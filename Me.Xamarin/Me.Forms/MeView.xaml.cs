096e17d97ad01af2b7a6f9262db596afd2eadfba
alcheByte <byron@alchebyte.com>
2019-10-30 10:50:57 -0500

    design: work on MeView

--- Me.Xamarin/Me.Forms/MeView.xaml.cs
+++ Me.Xamarin/Me.Forms/MeView.xaml.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Disposables;
 using Xamarin.Forms;
 
 using ReactiveUI.XamForms;
+using ReactiveUI;
+using System.IO;
 
 namespace Me.Forms
 {
@@ -20,6 +23,25 @@ namespace Me.Forms
         public MeView()
         {
             InitializeComponent();
+            this.WhenActivated(dispReg =>
+            {
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Picture,
+                    view => view.__Picture.Source,
+                    url => url == null ? null : ImageSource.FromStream(() => new MemoryStream(url)))
+                    .DisposeWith(dispReg);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.GivenNames,
+                    view => view.__GivenNames.Text,
+                    names => names == null ? "" : string.Join(" ", names))
+                    .DisposeWith(dispReg);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.FamilyName,
+                    view => view.__FamilyName.Text)
+                    .DisposeWith(dispReg);
+            });
         }
     }
 }
